ffe02a10c84ede6baca6dbb555c8d70e3f3037ba
Apurbo73 <apurbodebnath895@gmail.com>
2023-05-10 15:57:57 +0600

    Api Query Getting Related Videos

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api", // ekhane api na likhe dileo api dhore nibe. we can add anything we want.
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000"
+  }),
+  endpoints: builder => ({
+    getVideos: builder.query({
+      query: () => "/videos"
+    }),
+    getVideo: builder.query({
+      query: videoId => `/videos/${videoId}`
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split("");
+        const likes = tags.map(tag => `title_like=${tag}`);
+        const queryString = `/videos?${likes.join("&")} &_limit=4`;
+        return queryString;
+      }
+    })
+  })
+});
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery
+} = apiSlice;
